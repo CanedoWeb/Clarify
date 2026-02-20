5f6d86c3e60f69b1783418563db4339862afe991
CanedoWeb <canedodeveloper@gmail.com>
2026-02-20 15:41:11 -0300

    /

--- script.js
+++ script.js
@@ -5,16 +5,41 @@ document.addEventListener("DOMContentLoaded", () => {
         scrollTrigger: {
             trigger: '.divPai',
             start: 'top top',
-            end: '+=500',
-            scrub: 1.5,
+            end: '+=3000',
+            scrub: 2,
             pin: true
         }
     });
 
+    tl.addLabel("inicio", 0);
+
     tl.to('.divGradient', {
         opacity: 0,
+        duration: 10,
+        ease: "none"
+    }, "inicio");
+
+    tl.to('.divGradient2', {
+        opacity: 1,
+        duration: 10,
+        ease: "none"
+    }, "inicio");
+
+    tl.to('.tituloHero', {
+        opacity: 0,
+        duration: 2
+    }, "inicio");
+
+    const listaCards = document.querySelectorAll('.card')
+    listaCards.forEach((card, index) => {
+        tl.from(card, {
+            opacity: 0,
+            duration: 0.8
+        }, `inicio+=${index * 2}`);
+
+        tl.to(card, {
+            opacity: 0,
+            duration: 0.8
+        }, `inicio+=${index * 1.2 + 0.8}`);
     })
-        .to('.divGradient2', {
-            opacity: 1,
-        }, '<');
 });
