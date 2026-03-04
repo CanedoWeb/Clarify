6218f38e93810a2db616b5a5aabddb74566d3810
CanedoWeb <canedodeveloper@gmail.com>
2026-03-04 17:52:05 -0300

    .

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", () => {
         scrollTrigger: {
             trigger: '.divPai',
             start: 'top top',
-            end: "+=" + cards.length * 50 + "%",
+            end: "+=3000",
             scrub: 1.5,
             pin: true
         }
